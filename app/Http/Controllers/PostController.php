2d1b0fd9e8855f9e7df8d49c0be9ddb8dbcb56c8
Naga521 <teru-_-adini0521@ezweb.ne.jp>
2022-04-24 16:28:13 +0900

    [add]ブログ

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,29 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Post;
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest; // useする
 
 class PostController extends Controller
 {
-   public function index(Post $post)
-{
-    return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
-}
-    public function show(Post $post)
-   {
-      return view('posts/show')->with(['post' => $post]);
-   }
-   public function create()
-   {
-    return view('posts/create');
-   }
+    public function index(Post $post)
+    {
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+    }
 
-    public function store(Request $request, Post $post)//リクエストインスタンスの生成
-{
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+
+    public function create()
+    {
+        return view('posts/create');
+    }
+
+    public function store(Post $post, PostRequest $request) // 引数をRequest->PostRequestにする
     //データベースへ保存
-    $input = $request['post'];
-    $post->fill($input)->save();
-    return redirect('/posts/' . $post->id);
-}
-    
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
 }
